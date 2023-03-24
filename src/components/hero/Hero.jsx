30cde0a9a54007c2fe7853eedd151268e1b122fe
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-03-24 15:25:05 +0100

    24-03-2023

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,20 +1,58 @@
-import "./index.module.scss";
-import styles from "./index.module.scss";
+import styled from "styled-components";
+
+const HeroWrapped = styled.div`
+  position: relative;
+  height: 600px;
+  width: 100vw;
+  @media screen and (min-width: 800px) {
+    height: 700px;
+  }
+`;
+const Overlay = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 1;
+`;
+
+const Image = styled.img`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`;
+const Text = styled.div`
+  @font-face {
+    font-family: elegantFont;
+    src: url(/TheChamidPersonalUseOnly-J63a.ttf);
+  }
+
+  position: absolute;
+  top: 40%;
+  right: 20%;
+  font-size: 1.9rem;
+  z-index: 2;
+  font-family: "elegantFont", sans-serif; ;
+`;
+const Paragraf = styled.p`
+  font-style: italic;
+`;
 
 const Hero = () => {
   return (
-    <div className={styles.Hero}>
-      <div className={styles.overlay}></div>
-      <img
+    <HeroWrapped>
+      <Overlay />
+      <Image
         src="https://images.lifestyleasia.com/wp-content/uploads/sites/2/2022/11/02114003/whiskey-cocktails-2-1600x900.jpeg"
         alt="hero image"
-        className={styles.image}
       />
-      <div className={styles.text}>
+      <Text>
         <h1> Cocktails</h1>
-        <p>...drink responsibly</p>
-      </div>
-    </div>
+        <Paragraf>...drink responsibly</Paragraf>
+      </Text>
+    </HeroWrapped>
   );
 };
+
 export default Hero;
